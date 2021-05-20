c10c8e04cb5c27226529305764db1bc8dc3f6534
GabrielCB1 <gcbgabriel@gmail.com>
2021-05-20 13:46:40 -0500

    Archivos

---
+++ unit1/activity_01.py
@@ -0,0 +1,13 @@
+print ("start Program")
+
+num1= float(input("what's your first number?: "))
+num2 = float(input("What's your second number?: ")) 
+
+totalAdd= num1+num2
+totalMult= num1*num2
+
+print ("\n")
+print ("The sum of the two numbers is equal to: ",totalAdd,"\n")
+print ("The product of the two numbers is equal to: ",totalMult,"\n")
+
+print ("End Program")
